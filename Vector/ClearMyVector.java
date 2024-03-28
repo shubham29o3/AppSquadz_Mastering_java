25f4a05da303553c266ddb7907048ad0259d6297
Shubham Shrivastava <97184899+shubham29o3@users.noreply.github.com>
2024-03-28 15:09:33 +0530

    Vector clear() Method
    Using the clear() method only clears all the element from the vector and does not delete the vector. In other words, we can say that the clear() method is used to only empty an existing vector.

---
+++ Vector/ClearMyVector.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ClearMyVector {
+    public static void main(String[] args) {
+        Vector<String> vct = new Vector<String>();
+        //adding elements to the end
+        vct.add("First");
+        vct.add("Second");
+        vct.add("Third");
+        vct.add("Random");
+        System.out.println("Actual vector: " + vct);
+        vct.clear();
+        System.out.println("After clear vector: "+ vct );
+    }
+}
