76a60885c9ab0736b7318f65995cfdef475efd21
Shubham Shrivastava <97184899+shubham29o3@users.noreply.github.com>
2024-03-28 15:02:46 +0530

    Vector basic Operation
    The code initializes and manipulates a Vector in Java, demonstrating methods like adding, removing elements, iterating using Enumeration and Iterator, and trimming capacity.

---
+++ Vector/VectorDemo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<String> al = new Vector<>(3,2);
+        System.out.println("initial Capacity: "+al.capacity());
+        System.out.println(al.size());
+        al.addElement("a");
+        al.addElement("b");
+        al.addElement("a");
+        al.addElement("b");
+        System.out.println("capacity: " + al.capacity());
+        System.out.println(al.size());
+        al.removeElementAt(2);
+        Enumeration<String> e1 = al.elements();
+        System.out.println(e1.getClass().getName()); //nested class(anonymous)
+        while(e1.hasMoreElements()){
+
+            String s = e1.nextElement();
+            System.out.print(s);
+        }
+        System.out.println(" ");
+        Iterator<String> e = al.iterator();
+        while (e.hasNext()){
+            String s = e.next();
+            System.out.print(s);
+        }
+        al.trimToSize();
+        System.out.println(("\ncapacity : " + al.capacity()));
+    }
+}
