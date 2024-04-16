3c1eb1efa6fa08cab98b8f988b73f8cfa5d64894
Shubham Shrivastava <97184899+shubham29o3@users.noreply.github.com>
2024-04-16 14:16:13 +0530

    creating a CallableStatement by preparing a SQL call.

--- JDBC/CallableStatement/Callp.java
+++ JDBC/CallableStatement/Callp.java
@@ -1,3 +1,24 @@
-
+import java.sql.*;
+import java.io.*;
 public class Callp {
+    public static void main(String[] args) {
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","abc");
+            Statement s = conn.createStatement();
+            CallableStatement cs = conn.prepareCall("{call recins88(?,?)}");
+            cs.setInt(1,41);
+            cs.setString(2,"12april");
+
+            cs.executeUpdate();
+
+            ResultSet result1 = s.executeQuery("select * from emp65");
+            while(result1.next()){
+                System.out.println(result1.getString(1));
+                System.out.println(result1.getString(2));
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
